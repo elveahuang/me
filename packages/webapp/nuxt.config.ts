da2b99c3bd26c65cfcc7cae7f1b52f1f74bf9fdd
Elvea Huang <elvea.huang@gmail.com>
2026-09-18 22:13:38 +0800

    dev

--- packages/webapp/nuxt.config.ts
+++ packages/webapp/nuxt.config.ts
@@ -19,10 +19,6 @@ export default defineNuxtConfig({
     runtimeConfig: {
         deepseekApiKey: '',
     },
-    // @nuxt/ui 默认会启用 @nuxt/fonts；其底层 unifont 需要拉取各字体源的元数据索引。
-    // Google 字体元数据宿主 fonts.google.com 在当前网络下不可达，会触发
-    // "Could not fetch from ... retries left" 的 WARN 并拖慢 dev 启动。
-    // 项目未使用 Google 字体，禁用该 provider 即可（其他字体源仍可用）。
     fonts: {
         providers: {
             google: false,
@@ -32,6 +28,11 @@ export default defineNuxtConfig({
     icon: {
         serverBundle: {
             collections: ['mdi'],
+        },
+    },
+    vite: {
+        optimizeDeps: {
+            exclude: ['@nuxtjs/mdc'],
         },
     },
 });
